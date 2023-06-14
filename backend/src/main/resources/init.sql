734b6cb2aa158633b354e61afaa4eb3054bf3490
Jona Fleckenstein <j.fleckenstein@micromata.de>
2023-06-14 12:34:31 +0200

    init db data

---
+++ backend/src/main/resources/init.sql
@@ -0,0 +1,11 @@
+INSERT INTO Auto (id, kennzeichen, hersteller, typ, baujahr, ps, ccm, farbe, kraftstoff, sitzplaetze, extras, zubehoer, versicherungs_nr, tuev, asu, verliehen)
+VALUES
+    (1, 'KS R666', 'Renault', 'Megane', 2002, 100, 1596, 'weiß', 'Diesel', 5, 'Radio', '-', '3497856', '2005-05-01', '2005-05-01', false),
+    (2, 'KS JV417', 'Lancia', 'Thema', 2004, 145, 1999, 'schwarz', 'Normal', 5, 'Radio', 'Hupe', '3584,247', '2007-08-01', '2007-08-01', false),
+    (3, 'KS JS811', 'VW', 'Golf', 2003, 55, 1263, 'weiß', 'Normal', 5, 'Radio', 'Schiebedach', '655/060455-Q', '2005-11-01', '2005-11-01', false),
+    (4, 'KS TP14', 'Honda', 'CRX', 2006, 124, 1994, 'rot', 'Super', 2, 'Alufelgen', 'defektes Schiebedach', '2,5428414', '2007-06-01', '2007-06-01', false),
+    (5, 'KS K2863', 'Ford', 'Focus', 2006, 90, 1590, 'blau', 'Super', 5, 'Alu', 'Zentralverriegelung', '384+90+6467947', '2007-10-01', '2007-10-01', false),
+    (6, 'KS T1010', 'VW', 'Polo', 2001, 45, 1035, 'tornadorot', 'Normal', 5, 'Radio', '-', '9874684947', '2005-06-01', '2005-06-01', false),
+    (7, 'KS KV23', 'Ford', 'Focus', 2006, 54, 1597, 'matschschwarz', 'Diesel', 5, 'Radio', 'ZV', '654684354', '2007-09-01', '2007-09-01', false),
+    (8, 'KS MB220', 'Mercedes Benz', '220 S', 2006, 123, 2156, 'silbergrau', 'Super', 5, 'Radio', 'Navi', '65846454', '2007-04-01', '2007-04-01', false),
+    (9, 'KS CE221', 'VW', 'Golf', 2007, 75, 1596, 'rot', 'Normal', 5, 'Radio', '-', '55514-B1124', '2004-05-01', '2004-05-01', false);
