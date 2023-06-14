891c7fda77b0a5f1bdd40c4f72f883d7d85ae1a4
NikitaMic <91180248+NikitaMic@users.noreply.github.com>
2023-06-14 11:52:05 +0200

    brauchen wa net

--- backend/src/main/java/de/micromata/lf8_3/controller/AutoController.java
+++ backend/src/main/java/de/micromata/lf8_3/controller/AutoController.java
@@ -7,6 +7,9 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import de.micromata.lf8_3.repository.*;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -28,9 +31,34 @@ public class AutoController {
     autoRepository.save(auto);
   }
 
-  @PostMapping("/saveAuto")
-  public void addAuto(@RequestBody Auto auto) {
+  @DeleteMapping("/deleteAuto")
+  public void deleteAuto(@RequestBody Auto auto) {
+    autoRepository.delete(auto);
+  }
 
+  @PutMapping("/changeAuto")
+  public Auto getAllAutos(@RequestBody Auto updatedAuto) {
+    Auto auto = autoRepository.findById(updatedAuto.getId()).get();
+
+    auto.setKennzeichen(updatedAuto.getKennzeichen());
+    auto.setHersteller(updatedAuto.getHersteller());
+    auto.setTyp(updatedAuto.getTyp());
+    auto.setBaujahr(updatedAuto.getBaujahr());
+    auto.setPs(updatedAuto.getPs());
+    auto.setCcm(updatedAuto.getCcm());
+    auto.setFarbe(updatedAuto.getFarbe());
+    auto.setKraftstoff(updatedAuto.getKraftstoff());
+    auto.setSitzplaetze(updatedAuto.getSitzplaetze());
+    auto.setExtras(updatedAuto.getExtras());
+    auto.setZubehoer(updatedAuto.getZubehoer());
+    auto.setVersicherungsNr(updatedAuto.getVersicherungsNr());
+    auto.setTuev(updatedAuto.getTuev());
+    auto.setAsu(updatedAuto.getAsu());
+    auto.setVerliehen(updatedAuto.getVerliehen());
+    auto.setPreisgruppe_id(updatedAuto.getPreisgruppe_id());
+
+    autoRepository.save(auto);
+    return auto;
   }
 }
 
