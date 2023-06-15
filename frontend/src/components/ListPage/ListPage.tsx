fc05761f3b61ea8849fa463a4a1c46138e73bef1
Léon Spohr <l.spohr@micromata.de>
2023-06-15 10:05:40 +0200

    table stuff

--- frontend/src/components/ListPage/ListPage.tsx
+++ frontend/src/components/ListPage/ListPage.tsx
@@ -14,35 +14,35 @@ export const ListPage = () => {
 
   return (
       <div id="car-list">
-      <table>
-        <thead>
-        <tr>
-          <th>ID</th>
-          <th>Typ</th>
-          <th>€/Tag</th>
-          <th>€/km</th>
-        </tr>
-        </thead>
-        <tbody>
-        {allAutos && allAutos.map((auto: any) => (
-            <tr key={auto.id}>
-              <td>{auto.id}</td>
-              <td>{auto.typ}</td>
-              {auto.preisgruppe ?
-                  <>
-                    <td>{auto.preisgruppe.preis_tag}</td>
-                    <td>{auto.preisgruppe.preis_kilometer}</td>
-                  </>
-                  :
-                  <>
-                    <td></td>
-                    <td></td>
-                  </>
-              }
-            </tr>
-        ))}
-        </tbody>
-      </table>
-</div>
+        <table>
+          <thead>
+          <tr>
+            <th>ID</th>
+            <th>Typ</th>
+            <th>€/Tag</th>
+            <th>€/km</th>
+          </tr>
+          </thead>
+          <tbody>
+          {allAutos && allAutos.map((auto: any) => (
+              <tr className='clickable-row' key={auto.id}>
+                  <td>{auto.id}</td>
+                  <td>{auto.typ}</td>
+                  {auto.preisgruppe ?
+                      <>
+                        <td>{auto.preisgruppe.preis_tag}</td>
+                        <td>{auto.preisgruppe.preis_kilometer}</td>
+                      </>
+                      :
+                      <>
+                        <td></td>
+                        <td></td>
+                      </>
+                  }
+              </tr>
+          ))}
+          </tbody>
+        </table>
+      </div>
   );
 };
