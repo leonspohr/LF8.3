cf524a26cf965c416dd996a4aec67a2a39def5e7
Léon Spohr <l.spohr@micromata.de>
2023-06-16 08:17:47 +0200

    AutoPage styling

--- frontend/src/components/AutoPage/AutoPage.tsx
+++ frontend/src/components/AutoPage/AutoPage.tsx
@@ -14,41 +14,41 @@ export const AutoPage = (props:any) => {
   return (
       <div className="page--auto">
         <main>
-          {auto &&
-              <div>
+          {auto && (
+              <div className="auto-details">
                 <h1>{auto.hersteller} {auto.typ}</h1>
                 <ul>
-                  <li>id             {auto.id}</li>
-                  <li>kennzeichen    {auto.kennzeichen}</li>
-                  <li>baujahr        {auto.baujahr}</li>
-                  <li>ps             {auto.ps}</li>
-                  <li>ccm            {auto.ccm}</li>
-                  <li>farbe          {auto.farbe}</li>
-                  <li>kraftstoff     {auto.kraftstoff}</li>
-                  <li>sitzplaetze    {auto.sitzplaetze}</li>
-                  <li>extras         {auto.extras}</li>
-                  <li>zubehoer       {auto.zubehoer}</li>
-                  <li>versicherungsNr {auto.versicherungsNr}</li>
-                  <li>tuev           {auto.tuev}</li>
-                  <li>asu            {auto.asu}</li>
-                  <li>verliehen      {auto.verliehen ? 'verliehen' : "nicht verliehen"}</li>
-                  {auto.preisgruppe &&
+                  <li><span className="label">ID:</span> {auto.id}</li>
+                  <li><span className="label">Kennzeichen:</span> {auto.kennzeichen}</li>
+                  <li><span className="label">Baujahr:</span> {auto.baujahr}</li>
+                  <li><span className="label">PS:</span> {auto.ps}</li>
+                  <li><span className="label">ccm:</span> {auto.ccm}</li>
+                  <li><span className="label">Farbe:</span> {auto.farbe}</li>
+                  <li><span className="label">Kraftstoff:</span> {auto.kraftstoff}</li>
+                  <li><span className="label">Sitzplätze:</span> {auto.sitzplaetze}</li>
+                  <li><span className="label">Extras:</span> {auto.extras}</li>
+                  <li><span className="label">Zubehör:</span> {auto.zubehoer}</li>
+                  <li><span className="label">VersicherungsNr:</span> {auto.versicherungsNr}</li>
+                  <li><span className="label">TÜV:</span> {auto.tuev}</li>
+                  <li><span className="label">ASU:</span> {auto.asu}</li>
+                  <li><span className="label">Verliehen:</span> {auto.verliehen ? 'Verliehen' : 'Nicht verliehen'}</li>
+                  {auto.preisgruppe && (
                       <>
-                        <li>{auto.preisgruppe.preis_tag}</li>
-                        <li>{auto.preisgruppe.preis_kilometer}</li>
+                        <li><span className="label">€/Tag:</span> {auto.preisgruppe.preis_tag}</li>
+                        <li><span className="label">€/km:</span> {auto.preisgruppe.preis_kilometer}</li>
                       </>
-                  }
+                  )}
                 </ul>
 
-                  <button onClick={() => setVerliehen(auto.id, setAuto)}>
-                    Auto verleihen
-                  </button>
+                <button className="verleihen-btn" onClick={() => setVerliehen(auto.id, setAuto)}>
+                  Auto verleihen
+                </button>
               </div>
-          }
+          )}
         </main>
       </div>
   );
-}
+};
 
 
 
